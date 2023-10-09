92ef1246eef88ccc54cfb14d9f5eacba759ce654
Angel Romero <anro9968@colorado.edu>
2023-10-08 23:19:47 -0600

    data sets added

--- globalWarming.py
+++ globalWarming.py
@@ -2,11 +2,8 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-x = np.linspace(-3.14, 3.14, 360)
-y = np.linspace(-3.14, 3.14, 360)
+file = pd.read_csv('./data_sets/DATA.csv')
 
-plt.scatter(x, y)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Plot')
-plt.show()
+data = file.iloc[:, ]
+
+print(data)
